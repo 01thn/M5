cfd1c694256d69c9590ce023b5bee1844c773f55
01thn <01thn@github.com>
2020-08-24 11:59:42 +0300

    New version

--- scriptlog.js
+++
@@ -1,6 +0,0 @@
-let trueLog = 'admin';
-let truePass = 'admin';
-function getData() {
-if (document.getElementById('log').value == trueLog && document.getElementById('pass').value == truePass) {window.open("index.html");}
-else {alert('Something wrong')};
-}
